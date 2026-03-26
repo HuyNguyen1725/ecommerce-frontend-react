20854d8e869031c567f922bb7b0940456532e4c8
HuyNguyen1725 <0702huy@gmail.com>
2026-03-26 22:57:02 +0700

    fix media_url

--- src/component/Home.js
+++ src/component/Home.js
@@ -84,7 +84,7 @@ function Home() {
             <div className="product-image-wrapper">
               <div className="single-products">
                 <div className="productinfo text-center">
-                  <img src={`${process.env.REACT_APP_API_URL}media/products/${prd.image[0]}`} alt="" />
+                  <img src={`https://api.huynguyen1725.com/media/products/${prd.image[0]}`} alt="" />
                   <h2>${prd.price}</h2>
                   <p>{prd.productname}</p>
                   <button onClick={handleAddToCart} className="btn btn-default add-to-cart"><i className="fa fa-shopping-cart" />Add to cart</button>
